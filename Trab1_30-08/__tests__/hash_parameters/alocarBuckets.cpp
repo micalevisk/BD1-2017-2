bb12ef3438d45656faae23962e5a26f5356649d3
icomp <mllc@icomp.ufam.edu.br>
2017-08-25 10:30:28 -0400

    bacana

--- Trab1_30-08/__tests__/hash_parameters/alocarBuckets.cpp
+++ Trab1_30-08/__tests__/hash_parameters/alocarBuckets.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <fstream>
 #include <sys/stat.h>
+#include <cstring>
 
 
 #define ARQUIVO_HASH "__hashfile"
@@ -27,14 +28,27 @@ int main()
   fstream arqDados(ARQUIVO_HASH, fstream::in | fstream::out | fstream::trunc | ios::binary);
   if(!arqDados.is_open()) return 1;
 
-  Bloco bufferBloco;
+  Bloco bufferBloco = { .qtdRegistros = 0 };
+  // strcpy(bufferBloco.dados, "some data");
+  bufferBloco.dados[0] = '\0';
 
-  for(unsigned i=0; i < QTD_BUCKETS; ++i)
+  // escrevendo os buckets no arquivo
+  for(unsigned i=0; i < QTD_BUCKETS; ++i){
     arqDados.write((char*)&bufferBloco, sizeof(Bloco) * QTD_BLOCOS_POR_BUCKET);
+    bufferBloco.qtdRegistros = 1;
+  }
+
+  // lendo blocos escritos
+  arqDados.seekg(0 * sizeof(Bloco), ios::beg);// posição do primeiro bloco do primeiro bucket
+  arqDados.read((char*)&bufferBloco, sizeof(Bloco));
+  cout << "O primeiro bloco contém " << bufferBloco.qtdRegistros << " registros" << endl;
+  arqDados.seekg(1 * sizeof(Bloco), ios::beg);// posição do segundo bloco do primeiro bucket (ou primeiro bloco do segundo bucket)
+  arqDados.read((char*)&bufferBloco, sizeof(Bloco));
+  cout << "O segundo bloco contém " << bufferBloco.qtdRegistros << " registros" << endl;
 
   arqDados.close();
 
-  struct stat results;// http://pubs.opengroup.org/onlinepubs/009695399/basedefs/sys/stat.h.html
+  struct stat results;
   if (!stat(ARQUIVO_HASH, &results)) {
    cout << "Análise do arquivo:";
    printf("\n\tThe file size in GB: %f", results.st_size / 1000000000.0);
