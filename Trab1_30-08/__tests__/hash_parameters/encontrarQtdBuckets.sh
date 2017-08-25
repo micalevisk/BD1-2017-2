e629cbd6ab4fcf7ecec58d3d91cfa617bc8d9e5b
micalevisk <mllc@icomp.ufam.edu.br>
2017-08-25 00:16:54 -0400

    menos estático

--- Trab1_30-08/__tests__/hash_parameters/encontrarQtdBuckets.sh
+++ Trab1_30-08/__tests__/hash_parameters/encontrarQtdBuckets.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# função hash: valor % quantidade_de_buckets
+FUNCAO_HASH="{ print \$0 % qtdbuckets }"
+
+
 : '
 Admitindo:
 1 bloco tem 4096 bytes
@@ -7,20 +11,23 @@ Admitindo:
 
 O retorno deste script serve para verificar
 quantos blocos irão colidir, i.e., ir para
-o mesmo bucket,
-dado uma determinada quantia de buckets.
+o mesmo bucket, dado uma determinada quantia de buckets.
 '
 
 QTD_BUCKETS=${1:-215000}
+TAM_BLOCO=4097 # igual ao tamanho de um registro pois cada bloco guardará apenas 1 registro
 
-awk -v qtdbuckets="$QTD_BUCKETS" '{ print $0 % qtdbuckets }' only-ids.txt |
-#               ^- função hash sobre o ID
-awk '{ ocorrencias[$0]++ } ; END { for(id in ocorrencias) print id ":" ocorrencias[id] }' |
-sort -k2 -t':' -n |
-tail -n1 |
-## retorna <id>:<quantidade_maxima_blocos>
-sed -r "s/.+:(.+)$/(em $QTD_BUCKETS buckets) pelo menos um contém \1 blocos./"
 
+QTD_BLOCOS_POR_BUCKET=$(
+  awk -v qtdbuckets="$QTD_BUCKETS" "$FUNCAO_HASH" only-ids.txt |
+  awk '{ ocorrencias[$0]++ } ; END { for(id in ocorrencias) print id ":" ocorrencias[id] }' |
+  sort -k2 -t':' -n |
+  tail -n1 |
+  ## retorna <id>:<quantidade_maxima_blocos>
+  cut -d: -f2
+)
+echo "(em $QTD_BUCKETS) pelo menos um contém $QTD_BLOCOS_POR_BUCKET blocos/registros de $TAM_BLOCO bytes"
+echo "Serão reservados $(( QTD_BUCKETS * (QTD_BLOCOS_POR_BUCKET*TAM_BLOCO) )) bytes"
 
 
 # cut -d';' -f1 ../../_artigo.csv |
