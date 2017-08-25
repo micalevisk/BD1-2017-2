fd2bbbbda9c64bb6500a212c9e8fad9c4a9a6eb0
icomp <mllc@icomp.ufam.edu.br>
2017-08-25 10:03:17 -0400

    fixed

--- Trab1_30-08/__tests__/hash_parameters/encontrarQtdBuckets.sh
+++ Trab1_30-08/__tests__/hash_parameters/encontrarQtdBuckets.sh
@@ -15,10 +15,10 @@ o mesmo bucket, dado uma determinada quantia de buckets.
 '
 
 QTD_BUCKETS=${1:-215000}
-TAM_BLOCO=4097 # igual ao tamanho de um registro pois cada bloco guardará apenas 1 registro
+TAM_BLOCO=4096 # igual ao tamanho de um registro pois cada bloco guardará apenas 1 registro
 
-
-QTD_BLOCOS_POR_BUCKET=$(
+# maior número de colisões
+QTD_COLISOES=$(
   awk -v qtdbuckets="$QTD_BUCKETS" "$FUNCAO_HASH" only-ids.txt |
   awk '{ ocorrencias[$0]++ } ; END { for(id in ocorrencias) print id ":" ocorrencias[id] }' |
   sort -k2 -t':' -n |
@@ -26,8 +26,14 @@ QTD_BLOCOS_POR_BUCKET=$(
   ## retorna <id>:<quantidade_maxima_blocos>
   cut -d: -f2
 )
-echo "(em $QTD_BUCKETS) pelo menos um contém $QTD_BLOCOS_POR_BUCKET blocos/registros de $TAM_BLOCO bytes"
-echo "Serão reservados $(( QTD_BUCKETS * (QTD_BLOCOS_POR_BUCKET*TAM_BLOCO) )) bytes"
+
+echo "(em $QTD_BUCKETS) pelo menos um contém $QTD_COLISOES registros de $TAM_BLOCO bytes"
+
+QTD_BLOCOS_POR_BUCKET=1
+QTD_REGISTROS_POR_BLOCO=7
+
+TAM_BUCKET=$(( QTD_BLOCOS_POR_BUCKET * TAM_BLOCO ))
+echo "Serão reservados $(( TAM_BUCKET * QTD_BUCKETS )) bytes"
 
 
 # cut -d';' -f1 ../../_artigo.csv |
