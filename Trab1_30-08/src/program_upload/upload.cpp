a6a8c43cc0a81fb336cb0196f9cd5dc3263928d9
micalevisk <mllc@icomp.ufam.edu.br>
2017-08-30 23:20:44 -0400

    criação do arquivo de dados em hash DONE

--- Trab1_30-08/src/program_upload/upload.cpp
+++ Trab1_30-08/src/program_upload/upload.cpp
@@ -3,16 +3,27 @@
 //  Created by Micael Levi on 2017-08-26
 //  Copyright (c) 2017 mllc@icomp.ufam.edu.br; All rights reserved.
 //
-//  A priori, recebe o caminho para o arquivo CSV
-//  para criar um arquivo binário,
-//  o arquivo de dados, usando hashing estática.
+//  Recebe o caminho para o arquivo CSV
+//  para criar três arquivos binários
+//  o arquivo de dados, usando hashing estática;
+//  o arquivo de índice primário, usando B-Tree indexada pelo ID;
+//  o arquivo de índice secundário, usando B-Tree indexada pelo TITULO;
 //  Dessa forma ele fará a carga inicial da massa de dados de testes.
+//
+//  Para usar este programa, compile:
+//  $ g++ -std=c++11 upload.cpp -o upload
+//  E execute:
+//  upload <path/to/data.csv>
 //
 
 
+#define SHOW_VARIABLE(x) (std::cerr << #x " = " << (x) << '\n')
+
 // ---------------------------------------------------------------------------------------- //
 
-#define PATH_HASH_FILE "__hashfile"
+#define DIR_ARQUIVOS_CRIADOS "../../arquivos_criados/"
+
+#define PATH_HASH_FILE DIR_ARQUIVOS_CRIADOS"__hashfile"
 
 // parâmetros para a hash externa
 #define QTD_BUCKETS 1549147 // M
@@ -23,6 +34,7 @@
 // definição dos tamanhos
 #define ARTIGO_SIZE (sizeof(Artigo)) // R
 #define BLOCO_SIZE (sizeof(Bloco)) // B
+#define QTD_CAMPOS_ARTIGO 7
 
 
 #ifdef _REAL_ // caso seja o cenário real (especificado)
@@ -74,22 +86,79 @@ struct Bloco {//4096 bytes
 
 
 // ---------------------------------------------------------------------------------------- //
-template<const char delimiter = ';'> std::string getFieldFrom(std::ifstream& stream, bool& validField);
-auto getRecordFrom(std::ifstream& inputStream) -> Artigo*;
+template<const char delimiter> std::string getFieldFrom(std::ifstream&, bool&);
+template<typename F> bool insertRecordOnHashFile(std::fstream&, const Artigo&, F);
 // ---------------------------------------------------------------------------------------- //
 
-
 // ---------------------------------------------------------------------------------------- //
-unsigned bucketPara(const Artigo& artigo){
-  // aplicar função hash
-  return artigo.id % QTD_BUCKETS;
+Artigo* getRecordArtigoFrom(std::ifstream& inputStream){ // leitura específica para o tipo Artigo
+  bool algumRegistroValido = false;
+  Artigo* record = new Artigo;
+
+  for (unsigned qtdCamposLidos=0; qtdCamposLidos < QTD_CAMPOS_ARTIGO; ) {
+    bool campoValido = false;
+    std::string campoCurr = getFieldFrom<';'>(inputStream, campoValido);
+
+    if (!campoValido ) {
+      if (inputStream.eof()) break;
+      continue;
+    }
+
+    algumRegistroValido = true;
+    switch (qtdCamposLidos) {
+      case 0:
+        record->id = StringUtils::removeDoubleQuotesAndParseInt(campoCurr);
+        break;
+
+      case 1:
+        StringUtils::stringToCharArray(campoCurr, record->titulo, ARTIGO_TITULO_MAX_SIZE);
+        break;
+
+      case 2:
+        record->ano = StringUtils::removeDoubleQuotesAndParseInt(campoCurr);
+        break;
+
+      case 3:
+        StringUtils::stringToCharArray(campoCurr, record->autores, ARTIGO_ATORES_MAX_SIZE);
+        break;
+
+      case 4:
+        record->citacoes = StringUtils::removeDoubleQuotesAndParseInt(campoCurr);
+        break;
+
+      case 5:
+        StringUtils::stringToCharArray(campoCurr, record->atualizacao, ARTIGO_ATUALIZACAO_MAX_SIZE);
+        break;
+
+      case 6:
+        StringUtils::stringToCharArray(campoCurr, record->snippet, ARTIGO_SNIPPET_MAX_SIZE);
+
+      default:;
+      }
+
+      qtdCamposLidos++;
+  }
+
+  if (!algumRegistroValido) {
+    delete[] record;
+    record = NULL;
+  }
+
+  return record;
 }
 // ---------------------------------------------------------------------------------------- //
 
 
 using namespace std;
 
-int main(const int argc, const char *argv[])
+
+
+// O campo de hash escolhido deve ser retornado pela função abaixo.
+int getArtigoId(const Artigo& artigo){
+  return artigo.id;
+}
+
+int main(const int argc, const char* argv[])
 {
   if (argc != 2) return 1;// TODO mostrar help (como usar este programa)
 
@@ -97,27 +166,27 @@ int main(const int argc, const char *argv[])
   fstream arqDados;
   ifstream arqComDados; // arquivo que contém a hash e arquivo de entrada, respectivamente
 
-  /*
-  cout << "tamanho do Artigo = " << ARTIGO_SIZE << endl;
-  cout << "bfr = " << FATOR_BLOCO << endl;
-  cout << "tamanho do arquivo = " << BUCKET_SIZE * QTD_BUCKETS << endl;
-  */
-
 
   /* ===================== alocação do arquivo de dados ===================== */
   arqDados.open(PATH_HASH_FILE, fstream::in | fstream::out | fstream::trunc | ios::binary);
-  if (!arqDados.is_open()) exit(EXIT_FAILURE);// TODO tornar verboso
+  if (!arqDados.is_open()) {
+    fprintf(stderr, "{ERROR at %s, code line %d}\n", __FILE__, __LINE__);
+    exit(EXIT_FAILURE);// TODO tornar verboso
+  }
 
-  Bloco bufferBloco = { 0 }; // buffer pra Bloco (ou página, se estiver na MP) com 0 registros
+  Bloco bufferPage = { 0 }; // buffer pra Bloco (ou página, se estiver na MP) com 0 registros
   // escrevendo os buckets no arquivo
-  for(unsigned i=0; i < QTD_BUCKETS; ++i){
-    for(unsigned numBloco=0; numBloco < QTD_BLOCOS_POR_BUCKET; ++numBloco){
+  for (unsigned i=0; i < QTD_BUCKETS; ++i) {
+    // quando estão na memoria principal, blocos são chamados de "páginas"
+    for (unsigned numPagina=0; numPagina < QTD_BLOCOS_POR_BUCKET; ++numPagina) {
       // escrever 1 bloco, i.e., alocar espaço para os registros
-      arqDados.write((char*)&bufferBloco, BLOCO_SIZE);
+      arqDados.write((char*)&bufferPage, BLOCO_SIZE);
     }
   }
 
   #ifdef DEBUG
+  unsigned long qtdRegistrosLidos = 0;
+
   struct stat results;
   if ( !stat(PATH_HASH_FILE, &results) ) {
    fprintf(stderr, "Análise do arquivo:\n");
@@ -127,25 +196,42 @@ int main(const int argc, const char *argv[])
   }
   #endif
 
+
   /* ============ ler CSV e inserir registros no arquivo de dados ============ */
   arqComDados.open(PATH_ARQUIVO_COM_DADOS);
-  if (!arqComDados.is_open()) exit(EXIT_FAILURE);// TODO tornar verboso
-
-  unsigned artigos = 0;
-  while (!arqComDados.eof()) {
-    artigos++;
-    Artigo* artigo = getRecordFrom(arqComDados);
-    cout << bucketPara(*artigo) << endl;
-
+  if (!arqComDados.is_open()) {
+    fprintf(stderr, "{ERROR at %s, code line %d}\n", __FILE__, __LINE__);
+    exit(EXIT_FAILURE);// TODO tornar verboso
   }
 
-  cout << "LIDOS = " << artigos << endl;
+  while (!arqComDados.eof()) {
+    Artigo* artigo = getRecordArtigoFrom(arqComDados);
+    if (!artigo) continue;
+
+    try { // tentar inserir registro lido no arquivo de dados
+      if ( !insertRecordOnHashFile(arqDados, *artigo, getArtigoId) )
+        throw "ERROR ao inserir na hash";
+    } catch (const char* msg) {
+      fprintf(stderr, "{%s: at %s, code line %d}\n", msg, __FILE__, __LINE__);
+      exit(EXIT_FAILURE);
+    }
+
+    #ifdef DEBUG
+    qtdRegistrosLidos++;
+    #endif
+  }
+
+
+  #ifdef DEBUG
+  SHOW_VARIABLE(qtdRegistrosLidos);
+  #endif
 
 
   arqDados.close();
   arqComDados.close();
 
-  #ifdef DEBUG
+
+  #ifdef TEST
   remove(PATH_HASH_FILE);
   #endif
 }
@@ -154,11 +240,12 @@ int main(const int argc, const char *argv[])
 
 
 
-///////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////// CLASSE "CSVReader" //////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 
-
-template<const char delimiter = ';'>
-auto getFieldFrom(std::ifstream& stream, bool& validField) -> std::string { // XXX consegue lê apenas os 100 primeiros
+template<const char delimiter>
+std::string getFieldFrom(std::ifstream& stream, bool& validField){ // XXX consegue ler apenas os 100 primeiros
   std::string field;
   char lastChar, currChar;
   unsigned quotesAmount = 0;
@@ -203,50 +290,52 @@ auto getFieldFrom(std::ifstream& stream, bool& validField) -> std::string { // X
 }
 
 
-auto getRecordFrom(std::ifstream& inputStream) -> Artigo* { // TODO neutralizar retorno
-  Artigo* record = new Artigo;
-  bool algumRegistroValido = false;
 
-  for (unsigned qtdCamposLidos=0; qtdCamposLidos < 7; ) {
-    bool campoValido = false;
-    string campoCurr = getFieldFrom(inputStream, campoValido);
+////////////////////////////////////////////////////////////////////////////////
+///////////////////////// CLASSE "ExternalHash" ////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 
-    if (campoValido) {
-      algumRegistroValido = true;
-      switch (qtdCamposLidos) {
-        case 0:
-          record->id = StringUtils::removeDoubleQuotesAndParseInt(campoCurr);
-          break;
+template<typename F, typename T>
+unsigned long hashOf(T data, F getKeyFrom){
+  // aplicar função hash
+  return getKeyFrom(data) % QTD_BUCKETS;
+}
 
-        case 1:
-          StringUtils::stringToCharArray(campoCurr, record->titulo, ARTIGO_TITULO_MAX_SIZE);
-          break;
 
-        case 2:
-          record->ano = StringUtils::removeDoubleQuotesAndParseInt(campoCurr);
-          break;
+template<typename F>
+bool insertRecordOnHashFile(std::fstream& hashFile, const Artigo& artigo, F getKey){
 
-        case 3:
-          StringUtils::stringToCharArray(campoCurr, record->autores, ARTIGO_ATORES_MAX_SIZE);
-          break;
+  // calcular o bucket que será inserido
+  unsigned long bucketNumber = hashOf(artigo, getKey);
 
-        case 4:
-          record->citacoes = StringUtils::removeDoubleQuotesAndParseInt(campoCurr);
-          break;
+  // mover cursor do arquivo de dados para o bucket correspondente, i.e., byte: bucketNumber * (m * B)
+  hashFile.seekg(bucketNumber * QTD_BLOCOS_POR_BUCKET * BLOCO_SIZE, std::ios::beg);
 
-        case 5:
-          StringUtils::stringToCharArray(campoCurr, record->atualizacao, ARTIGO_ATUALIZACAO_MAX_SIZE);
-          break;
+  // carregar bloco(s) do disco enquanto não houver espaço para inserir, i.e., encontrar bloco com espaço
+  Bloco bufferPage = { 0 }; // quando o bloco está na memória, ele é chamado de "página"
+  unsigned currPage;
 
-        case 6:
-          StringUtils::stringToCharArray(campoCurr, record->snippet, ARTIGO_SNIPPET_MAX_SIZE);
+  for (currPage = 0; currPage < QTD_BLOCOS_POR_BUCKET; ++currPage) {
+    // ler bloco encontrado ao mover cursor
+    hashFile.read((char*)&bufferPage, BLOCO_SIZE);
 
-        default:;
-      }
-
-      qtdCamposLidos++;
-    } else if (inputStream.eof()) break;
+    // bloco válido encontrado;
+    // os próximos sempre estarão livres (se existirem) mas o primeiro livre encontrado é o escolhido
+    // ocasionando um crescimento top-down do bucket
+    if (bufferPage.qtdRegistros < FATOR_BLOCO) break; // para quebrar o loop e evitar o incremento de currPage
   }
 
-  return algumRegistroValido ? record : NULL;
+  // currPage sempre será menor que QTD_BLOCOS_POR_BUCKET se algum bloco válido foi encontrado
+  if (currPage >= QTD_BLOCOS_POR_BUCKET) return false;
+
+  // atualizar o bloco encontrado, i.e., inserir o novo registro (ainda em memória principal)
+  bufferPage.qtdRegistros++;
+  std::memcpy(&bufferPage.dados[currPage * ARTIGO_SIZE], (char*)&artigo, ARTIGO_SIZE);
+
+  // reescrevendo no arquivo de dados o bloco encontrado que foi atualizado
+  hashFile.seekp(- ((currPage+1) * BLOCO_SIZE), std::ios::cur); // voltar para o primeiro byte do bloco encontrado
+  hashFile.write((char*)&bufferPage, BLOCO_SIZE);
+
+
+  return true;
 }
