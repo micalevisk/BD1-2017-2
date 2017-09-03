8b437fae86e0d43189f5f08cdf02c1fcf6dcac92
micalevisk <mllc@icomp.ufam.edu.br>
2017-09-02 22:13:50 -0400

    definições "globais"

---
+++ Trab1_30-08/include/parametros.h
@@ -0,0 +1,24 @@
+//
+//  [parametros.h]
+//  Created by Micael Levi and Victor Meireles on 2017-08-11
+//  Copyright (c) 2017 mllc@icomp.ufam.edu.br All rights reserved.
+//
+//  Guarda valores/funções que são comum os programas desenvolvidos.
+//
+
+
+#ifndef PARAMETROS_H
+#define PARAMETROS_H
+
+
+
+// ============= CAMINHOS PARA OS ARQUIVOS QUE SERÃO CRIADOS ============= //
+#define DIR_ARQUIVOS_CRIADOS "docs/"
+
+#define PATH_HASH_FILE DIR_ARQUIVOS_CRIADOS"__hashfile"
+#define PATH_PRIMARY_INDEX_FILE DIR_ARQUIVOS_CRIADOS"__primary_index"
+#define PATH_SECONDARY_INDEX_FILE DIR_ARQUIVOS_CRIADOS"__secondary_index"
+
+
+
+#endif
