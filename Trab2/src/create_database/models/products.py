4ed795eae5463442981c8702bb4765e6ec1d6ec7
micalevisk <mllc@icomp.ufam.edu.br>
2017-10-03 23:35:05 -0400

    done

--- Trab2/src/create_database/models/products.py
+++ Trab2/src/create_database/models/products.py
@@ -1,8 +1,10 @@
 #
 #   Copyright (c) 2017. All rights reserved.
 #   DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
-#   Created by Micael Levi and Victor Meireles on 2017-10-03
+#   Created by Micael Levi on 2017-10-03
 #
+
+from psycopg2 import sql
 
 
 class Product:
@@ -18,35 +20,42 @@ class Product:
         self.count_categories = count_categories
         self.count_total_reviews = count_total_reviews
 
-        # self._similars = [] # lista de Similar
-        # self._categories = [] # lista de Category
-
-    '''
-    @property
-    def similars(self):
-        return self._similars
-
-    @similars.setter
-    def similars(self, similars_list):
-        if similars_list:
-            self.count_similars = len(similars_list)
-            self._similars = similars_list
-
-    @property
-    def categories(self):
-        return self._categories
-
-    @categories.setter
-    def categories(self, categories_list):
-        if categories_list:
-            self.count_categories = len(categories_list)
-            self._categories = categories_list
-    '''
-
     def __str__(self):
         return "Id:{id}, ASIN:'{asin}', title:'{title}', group:'{name_group}', salesrank:{salesrank}, similar:{count_similars}, categories:{count_categories}, totalreviews:{count_total_reviews}".format(**self.__dict__)
 
     __repr__ = __str__
+
+
+
+QUERY_CREATE_TABLE_PRODUCTS = """
+CREATE TABLE IF NOT EXISTS products
+(
+  id INT NOT NULL,
+  asin VARCHAR(10),
+  title VARCHAR(500),
+  name_group VARCHAR(5),
+  salesrank INT,
+  count_similars INT DEFAULT 0,
+  count_categories INT DEFAULT 0,
+  count_total_reviews INT DEFAULT 0
+);
+"""
+
+QUERY_ADD_CONSTRAINTS_PRODUCTS = """
+ALTER TABLE products
+  ADD CONSTRAINT products_id UNIQUE (id),
+  ADD CONSTRAINT products_pkey PRIMARY KEY (asin);
+"""
+
+QUERY_TEMPLATE_INSERT_PRODUCT = """
+INSERT INTO products (id, asin, title, name_group, salesrank, count_similars, count_categories, count_total_reviews)
+SELECT {0}, %s, %s, %s, %s, %s, %s, %s
+WHERE NOT EXISTS (
+  SELECT asin FROM products
+  WHERE
+    id = {0} AND asin = %s
+);
+"""
 
 
 class ProductDAO:
@@ -54,5 +63,22 @@ class ProductDAO:
     def __init__(self, dbconnection):
         self._db = dbconnection
 
-    def createTable_products(self):
-        return ''
+    def createTable(self, doCommit=False):
+        return self._db.manipular(QUERY_CREATE_TABLE_PRODUCTS, None, doCommit, True)
+
+    def addConstraints(self, doCommit=False):
+        return self._db.manipular(QUERY_ADD_CONSTRAINTS_PRODUCTS, None, doCommit)
+
+    def insert(self, product:Product, doCommit=False):
+        queryTemplate = sql.SQL(QUERY_TEMPLATE_INSERT_PRODUCT).format( sql.Literal(product.id) )
+        return self._db.manipular(queryTemplate, [
+            product.asin,
+            product.title,
+            product.name_group,
+            product.salesrank,
+            product.count_similars,
+            product.count_categories,
+            product.count_total_reviews,
+
+            product.asin
+        ], doCommit, True)
--- Trab2/src/create_database/models/similars.py
+++ Trab2/src/create_database/models/similars.py
@@ -1,8 +1,10 @@
 #
 #   Copyright (c) 2017. All rights reserved.
 #   DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
-#   Created by Micael Levi and Victor Meireles on 2017-10-03
+#   Created by Micael Levi on 2017-10-03
 #
+
+from psycopg2 import sql
 
 
 class Similar:
@@ -17,7 +19,48 @@ class Similar:
     __repr__ = __str__
 
 
+
+QUERY_CREATE_TABLE_SIMILARS = """
+CREATE TABLE IF NOT EXISTS similars
+(
+  asin VARCHAR(10),
+  asin_similar VARCHAR(10) NOT NULL
+);
+"""
+
+QUERY_ADD_CONSTRAINTS_SIMILARS = """
+ALTER TABLE similars
+  ADD CONSTRAINT reviews_asin_asin_similar_key UNIQUE (asin, asin_similar),
+  ADD CONSTRAINT similars_products_asin_fk FOREIGN KEY (asin) REFERENCES products (asin) ON DELETE CASCADE;
+"""
+
+QUERY_TEMPLATE_INSERT_SIMILAR = """
+INSERT INTO similars (asin, asin_similar)
+SELECT %s, %s
+WHERE NOT EXISTS (
+  SELECT asin FROM similars
+  WHERE
+    asin = %s AND asin_similar = %s
+);
+"""
+
+
 class SimilarDAO:
     """ """
     def __init__(self, dbconnection):
-        self.dbconnection = dbconnection
+        self._db = dbconnection
+
+    def createTable(self, doCommit=False):
+        return self._db.manipular(QUERY_CREATE_TABLE_SIMILARS, None, doCommit, True)
+
+    def addConstraints(self, doCommit=False):
+        return self._db.manipular(QUERY_ADD_CONSTRAINTS_SIMILARS , None, doCommit)
+
+    def insert(self, similar:Similar, doCommit=False):
+        return self._db.manipular(QUERY_TEMPLATE_INSERT_SIMILAR, [
+            similar.asin,
+            similar.asin_similar,
+
+            similar.asin,
+            similar.asin_similar
+        ], doCommit, True)
